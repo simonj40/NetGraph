430c8c1a3792742bb47991389d6e68d36f6f0091
Jedi Salil Junior <saliljnr@gmail.com>
2015-01-05 13:43:57 +0100

    Commit from Japheth - with modifications NOTE: changed package name to ece.controller...

--- fr/ece/controller/Controller.java
+++ fr/ece/controller/Controller.java
@@ -1,18 +1,17 @@
-package fr.ece.controller;
+package ece.controller;
 
 import java.io.*;
 
 public class Controller {
 
-	private static String OS = System.getProperty("os.name").toLowerCase();
-	
+	private static String osName = System.getProperty("os.name").toLowerCase();
+	private static String address = "google.fr";
+
 	
 	public static void main(String[] args) {
-		
 
-			Traceroute tr = new Traceroute();
+			Traceroute tr = new Traceroute(osName, address);
 			tr.start();
-		
 		
 	}
 	
--- fr/ece/controller/Traceroute.java
+++ fr/ece/controller/Traceroute.java
@@ -1,4 +1,4 @@
-package fr.ece.controller;
+package ece.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,41 +10,52 @@ public class Traceroute extends Thread {
 	String traceroute ="";
 	
 	
-	private static String OS = System.getProperty("os.name").toLowerCase();
+	private static String osName;
+	private static String address;
+
+	/*
+	Constructor for Traceroute class
+	 */
+	Traceroute(String osName, String address){
+		this.osName = osName;
+		this.address = address;
+
+	}
 	
 	public void run() {
 		
-		String s = null;
-		
-		
-		
-		if( OS.indexOf("win") >= 0 ){
+		if( this.osName.indexOf("win") >= 0 ){
 			System.out.println("This is Windows");
+			tracer("tracert " + this.address);
 		}else{
 			System.out.println("This is not Windows");
-			
-			try {
-				Process p = Runtime.getRuntime().exec("traceroute google.fr");
-				
-				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		 
-		        BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-				
-		        while ((s = input.readLine()) != null) {
-		        	System.out.println(s);
-		        	traceroute += s;
-		        }
-		        System.out.println("RESPONSE");
-		        System.out.println(traceroute);
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+			tracer("traceroute " + this.address);
 		}
 		
 		
+	}
+
+	private void tracer(String command){
+		String s = null;
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+
+			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+			while ((s = input.readLine()) != null) {
+				System.out.println(s);
+				traceroute += s;
+			}
+			System.out.println("RESPONSE");
+			System.out.println(traceroute);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 	
 	
